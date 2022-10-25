1bb3d36f127be34f9e1b5b3c9b8307bd3eb56fd4
Alexander A. Klimov <alexander.klimov@icinga.com>
2022-10-25 17:45:48 +0200

    Write Icinga config

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,16 @@ use serde::Deserialize;
 use serde_json::Map;
 use serde_json::Value;
 use serde_json::from_str;
+use serde_json::to_string;
 use std::io::Error;
 use std::io::ErrorKind::ConnectionAborted;
 use std::io::ErrorKind::Unsupported;
 use std::io::Read;
 use std::io::Result;
 use std::io::StdinLock;
+use std::io::Write;
 use std::io::stdin;
+use std::io::stdout;
 use std::net::Shutdown as ShutdownMode;
 
 struct ShutdownableStdin(StdinLock<'static>);
@@ -36,12 +39,14 @@ struct Object {
 
 fn main() -> Result<()> {
     let mut stdin = ShutdownableStdin(stdin().lock());
+    let mut stdout = stdout().lock();
 
     loop {
         match stdin.read_netstring() {
             Ok(str) => {
                 let o: Object = from_str(str.as_str())?;
-                println!("{} {}", o.r#type, o.name);
+
+                write_icinga_object(&mut stdout, &o)?;
             }
             Err(err) => {
                 return match err.kind() {
@@ -51,4 +56,76 @@ fn main() -> Result<()> {
             }
         }
     }
+}
+
+fn write_icinga_object(w: &mut impl Write, o: &Object) -> Result<()> {
+    w.write("object ".as_ref())?;
+    w.write(o.r#type.as_bytes())?;
+    w.write(" ".as_ref())?;
+    write_icinga_str(w, &o.name)?;
+    w.write(" {\n".as_ref())?;
+
+    for (attr, val) in o.properties.iter() {
+        match attr.as_str() {
+            "__name" => {}
+            "name" => {}
+            "package" => {}
+            "source_location" => {}
+            "templates" => {}
+            "type" => {}
+            _ => {
+                w.write("  ".as_ref())?;
+                w.write(attr.as_bytes())?;
+                w.write(" = ".as_ref())?;
+                write_icinga_val(w, val)?;
+                w.write("\n".as_ref())?;
+            }
+        }
+    }
+
+    w.write("}\n\n".as_ref())?;
+
+    Ok(())
+}
+
+fn write_icinga_val(w: &mut impl Write, v: &Value) -> Result<()> {
+    match v {
+        Value::String(s) => { write_icinga_str(w, s)?; }
+
+        Value::Array(arr) => {
+            w.write("[ ".as_ref())?;
+
+            for vv in arr.iter() {
+                write_icinga_val(w, vv)?;
+                w.write(", ".as_ref())?;
+            }
+
+            w.write("]".as_ref())?;
+        }
+
+        Value::Object(obj) => {
+            w.write("{ ".as_ref())?;
+
+            for (k, v) in obj.iter() {
+                write_icinga_str(w, k)?;
+                w.write(" = ".as_ref())?;
+                write_icinga_val(w, v)?;
+                w.write("; ".as_ref())?;
+            }
+
+            w.write("}".as_ref())?;
+        }
+
+        _ => { w.write(to_string(v)?.as_bytes())?; }
+    }
+
+    Ok(())
+}
+
+fn write_icinga_str(w: &mut impl Write, s: &String) -> Result<()> {
+    w.write("{{{".as_ref())?;
+    w.write(s.as_bytes())?;
+    w.write("}}}".as_ref())?;
+
+    Ok(())
 }
